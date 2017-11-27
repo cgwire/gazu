df28813589092e83e655a5ad07944ed73a6b06fb
Frank Rousseau <frank.rousseau@free.fr>
2017-11-27 17:16:31 +0100

    Add helpers to retrieve a given person
    * Allow to retrieve it based on his desktop login
    * Add a function to retrieve it from his desktop login

--- test/test_person.py
+++ test/test_person.py
@@ -10,17 +10,17 @@ class PersonTestCase(unittest.TestCase):
     def test_all(self):
         with requests_mock.mock() as mock:
             mock.get(
-                gazu.client.get_full_url('data/persons'),
-                text='[{"first_name": "John", "id": "person-1"}]'
+                gazu.client.get_full_url("data/persons"),
+                text=json.dumps([{"first_name": "John", "id": "person-1"}])
             )
             persons = gazu.person.all()
             person_instance = persons[0]
             self.assertEquals(person_instance["first_name"], "John")
 
-    def test_get_person_by_name(self):
+    def test_get_person_by_full_name(self):
         with requests_mock.mock() as mock:
             mock.get(
-                gazu.client.get_full_url('data/persons'),
+                gazu.client.get_full_url("data/persons"),
                 text=json.dumps([
                     {
                         "first_name": "John",
@@ -39,5 +39,43 @@ class PersonTestCase(unittest.TestCase):
                     }
                 ])
             )
-            person = gazu.person.get_person_by_name("john.did")
+            person = gazu.person.get_person_by_full_name("John Did")
             self.assertEquals(person["id"], "person-2")
+
+    def test_get_person_by_desktop_login(self):
+        with requests_mock.mock() as mock:
+            mock.get(
+                gazu.client.get_full_url("data/persons?desktop_login=john.doe"),
+                text=json.dumps([
+                    {
+                        "first_name": "John",
+                        "last_name": "Doe",
+                        "desktop_login": "john.doe",
+                        "id": "person-1"
+                    }
+                ])
+            )
+            person = gazu.person.get_person_by_desktop_login("john.doe")
+            self.assertEquals(person["id"], "person-1")
+
+    def test_get_person_list(self):
+        with requests_mock.mock() as mock:
+            mock.get(
+                gazu.client.get_full_url("auth/person-list"),
+                text=json.dumps([
+                    {
+                        "first_name": "John",
+                        "last_name": "Doe",
+                        "desktop_login": "john.doe",
+                        "id": "person-1"
+                    },
+                    {
+                        "first_name": "Jane",
+                        "last_name": "Doe",
+                        "desktop_login": "jane.doe",
+                        "id": "person-1"
+                    }
+                ])
+            )
+            persons = gazu.person.get_simple_person_list()
+            self.assertEquals(persons[0]["id"], "person-1")
