0d51c475b09be5e16c9d076f5ba60f9da1cbfa27
Frank Rousseau <frank.rousseau@free.fr>
2017-12-09 20:04:49 +0100

    Duplicate some information in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup()
+from gazu import __version__
+
+setup(
+    name="Gazu",
+    version=__version__,
+    install_requires=["requests==2.18.4"]
+)
