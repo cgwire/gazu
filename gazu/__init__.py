587b0397b655f2d3867e289dd4996b6e2eab94f6
Frank Rousseau <frank.rousseau@free.fr>
2018-10-18 19:12:10 +0200

    0.6.2

--- gazu/__init__.py
+++ gazu/__init__.py
@@ -1,1 +1,37 @@
-__version__ = "0.6.1"
+from . import client
+from . import cache
+from . import helpers
+
+from . import asset
+from . import context
+from . import entity
+from . import files
+from . import project
+from . import person
+from . import shot
+from . import task
+from . import user
+
+from .exception import AuthFailedException
+
+__version__ = '0.6.2'
+
+
+def get_host():
+    return client.get_host()
+
+
+def set_host(url):
+    client.set_host(url)
+
+
+def log_in(email, password):
+    tokens = client.post("auth/login", {
+        "email": email,
+        "password": password
+    })
+    if "login" in tokens and tokens["login"] == False:
+        raise AuthFailedException
+    else:
+        client.set_tokens(tokens)
+    return tokens
