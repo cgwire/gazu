6d7dfb49d675f3ece61bf3dbb421bdf7862aa5aa
Frank Rousseau <frank.rousseau@free.fr>
2019-08-13 19:34:35 +0200

    Bump gazu version to 0.6.16

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, CGWire'
 author = u'CGWire'
 
 # The short X.Y version
-version = u'0.6.13'
+version = u'0.6.16'
 # The full version, including alpha/beta/rc tags
-release = u'0.6.13'
+release = u'0.6.16'
 
 
 # -- General configuration ---------------------------------------------------
