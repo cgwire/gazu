74104b0e7612749ca7370d8bfc17a60420d560c0
Frank Rousseau <frank.rousseau@free.fr>
2020-01-20 12:46:37 +0100

    Up gazu version to 0.7.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, CGWire'
 author = u'CGWire'
 
 # The short X.Y version
-version = u'0.7.1'
+version = u'0.7.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.7.1'
+release = u'0.7.5'
 
 
 # -- General configuration ---------------------------------------------------
