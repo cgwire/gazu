dcce599036d7e7a5af4e86a2f2efa160b0c9237e
Frank Rousseau <frank.rousseau@free.fr>
2020-03-18 12:18:37 +0100

    Generate docs for 0.7.7 version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, CGWire'
 author = u'CGWire'
 
 # The short X.Y version
-version = u'0.7.5'
+version = u'0.7.7'
 # The full version, including alpha/beta/rc tags
-release = u'0.7.5'
+release = u'0.7.7'
 
 
 # -- General configuration ---------------------------------------------------
