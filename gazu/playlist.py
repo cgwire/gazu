0330b99deb6b8486a2ff01f819c5a61a561cfaf3
Frank Rousseau <frank.rousseau@free.fr>
2020-09-27 22:12:30 +0200

    [qa] Add missing client parameter to playlist module

--- gazu/playlist.py
+++ gazu/playlist.py
@@ -1,22 +1,25 @@
-from . import client
+from . import client as raw
+
 from .helpers import normalize_model_parameter
 from .sorting import sort_by_name
 
 from .cache import cache
 
+default = raw.default_client
+
 
 @cache
-def all_playlists():
+def all_playlists(client=default):
     """
     Returns:
         list: All playlists for all projects.
     """
 
-    return sort_by_name(client.fetch_all("playlists"))
+    return sort_by_name(raw.fetch_all("playlists", client=client))
 
 
 @cache
-def all_shots_for_playlist(playlist):
+def all_shots_for_playlist(playlist, client=default):
     """
     Args:
         playlist (str / dict): The playlist dict or the playlist ID.
@@ -26,12 +29,12 @@ def all_shots_for_playlist(playlist):
     """
 
     playlist = normalize_model_parameter(playlist)
-    playlist = client.fetch_one("playlists", playlist["id"])
+    playlist = raw.fetch_one("playlists", playlist["id"], client=client)
     return sort_by_name(playlist["shots"])
 
 
 @cache
-def all_playlists_for_project(project):
+def all_playlists_for_project(project, client=default):
     """
 
     Args:
@@ -43,12 +46,12 @@ def all_playlists_for_project(project):
 
     project = normalize_model_parameter(project)
     return sort_by_name(
-        client.fetch_all("projects/%s/playlists" % project["id"])
+        raw.fetch_all("projects/%s/playlists" % project["id"], client=client)
     )
 
 
 @cache
-def get_playlist(playlist):
+def get_playlist(playlist, client=default):
     """
     Args:
         playlist (str / dict): The playlist dict or the playlist ID.
@@ -58,11 +61,11 @@ def get_playlist(playlist):
     """
 
     playlist = normalize_model_parameter(playlist)
-    return client.fetch_one("playlists", playlist["id"])
+    return raw.fetch_one("playlists", playlist["id"], client=client)
 
 
 @cache
-def get_playlist_by_name(project, name):
+def get_playlist_by_name(project, name, client=default):
     """
     Args:
         project (str / dict): The project dict or the project ID.
@@ -72,14 +75,21 @@ def get_playlist_by_name(project, name):
         dict: Playlist matching given name for given project.
     """
     project = normalize_model_parameter(project)
-    playlists = all_playlists_for_project(project)
+    playlists = all_playlists_for_project(project, client=client)
     for playlist in playlists:
         if playlist["name"] == name:
             return playlist
     return None
 
 
-def new_playlist(project, name):
+def new_playlist(
+    project,
+    name,
+    episode=None,
+    for_entity="shot",
+    for_client=False,
+    client=default
+):
     """
     Create a new playlist in the database for given project.
 
@@ -91,14 +101,22 @@ def new_playlist(project, name):
         dict: Created playlist.
     """
     project = normalize_model_parameter(project)
-    data = {"name": name, "project_id": project["id"]}
-    playlist = get_playlist_by_name(project, name)
+    data = {
+        "name": name,
+        "project_id": project["id"],
+        "for_entity": for_entity,
+        "for_client": for_client
+    }
+    if episode is not None:
+        episode = normalize_model_parameter(episode)
+        data["episode_id"] = episode["id"]
+    playlist = get_playlist_by_name(project, name, client=client)
     if playlist is None:
-        playlist = client.post("data/playlists/", data)
+        playlist = raw.post("data/playlists/", data, client=client)
     return playlist
 
 
-def update_playlist(playlist):
+def update_playlist(playlist, client=default):
     """
     Save given playlist data into the API. Metadata are fully replaced by
     the ones set on given playlist.
@@ -109,4 +127,8 @@ def update_playlist(playlist):
     Returns:
         dict: Updated playlist.
     """
-    return client.put("data/playlists/%s" % playlist["id"], playlist)
+    return raw.put(
+        "data/playlists/%s" % playlist["id"],
+        playlist,
+        client=client
+    )
