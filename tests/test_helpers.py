f9370cab14dcfd9b046c1214b28370f76f5b1d36
Evan Blaudy <evan@cg-wire.com>
2021-08-25 16:27:11 +0200

    [qa] add missing tests with exceptions for helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,3 +16,17 @@ class TestCase(unittest.TestCase):
             {"id": fakeid("asset-01")},
         )
         self.assertIsNone(helpers.normalize_model_parameter(None))
+
+        class TestCantCastStr(object):
+            def __str__(self):
+                raise TypeError('Can not by stringified')
+
+        try:
+            helpers.normalize_model_parameter(TestCantCastStr())
+        except Exception as e:
+            self.assertIsInstance(e, ValueError)
+
+        try:
+            helpers.normalize_model_parameter('NOT_AN_UUID')
+        except Exception as e:
+            self.assertIsInstance(e, ValueError)
