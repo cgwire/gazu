6cdeeb5b99295f24e1a79c8ba7e329447ee3269e
Evan Blaudy <evan@cg-wire.com>
2021-09-01 01:37:36 +0200

    [qa] add missing tests for scene

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -4,7 +4,9 @@ import requests_mock
 
 import gazu.client
 import gazu.scene
+import gazu.context
 
+from utils import fakeid
 
 class SceneTestCase(unittest.TestCase):
     def test_get_scene(self):
@@ -45,6 +47,32 @@ class SceneTestCase(unittest.TestCase):
             self.assertEqual(scene_instance["name"], "Scene 01")
             self.assertEqual(scene_instance["project_id"], "project-01")
 
+    def test_all_scenes(self):
+        with requests_mock.mock() as mock:
+            mock.get(
+                gazu.client.get_full_url("data/projects/project-01/scenes"),
+                text=json.dumps(
+                    [{"name": "Scene 01", "project_id": "project-01"}]
+                ),
+            )
+            mock.get(
+                gazu.client.get_full_url("data/scenes"),
+                text=json.dumps(
+                    [{"name": "Scene 01", "project_id": "project-01"}]
+                ),
+            )
+            project = {"id": "project-01"}
+            scenes = gazu.context.all_scenes_for_project(project, False)
+            self.assertEqual(len(scenes), 1)
+            scene_instance = scenes[0]
+            self.assertEqual(scene_instance["name"], "Scene 01")
+            self.assertEqual(scene_instance["project_id"], "project-01")
+            scenes = gazu.context.all_scenes_for_project(project=None, user_context=False)
+            self.assertEqual(len(scenes), 1)
+            scene_instance = scenes[0]
+            self.assertEqual(scene_instance["name"], "Scene 01")
+            self.assertEqual(scene_instance["project_id"], "project-01")
+
     def test_all_scenes_for_sequence(self):
         with requests_mock.mock() as mock:
             mock.get(
@@ -60,7 +88,7 @@ class SceneTestCase(unittest.TestCase):
                 ),
             )
             sequence = {"id": "sequence-01"}
-            scenes = gazu.scene.all_scenes_for_sequence(sequence)
+            scenes = gazu.context.all_scenes_for_sequence(sequence, False)
             self.assertEqual(len(scenes), 1)
             scene_instance = scenes[0]
             self.assertEqual(scene_instance["name"], "Scene 01")
@@ -229,3 +257,28 @@ class SceneTestCase(unittest.TestCase):
                 instance, updated_name
             )
             self.assertEqual(instance["name"], updated_name)
+
+    def test_update_asset_instance_data(self):
+        with requests_mock.mock() as mock:
+            mock.put(
+                gazu.client.get_full_url('data/asset-instances/%s' % fakeid('asset-instance-1')),
+                text=json.dumps({
+                    'id': fakeid('asset-instance-1'),
+                    'name': 'asset-instance-1',
+                    'data': {'extra-data': 'extra-data'}
+                })
+            )
+            asset_instance = gazu.scene.update_asset_instance_data(fakeid('asset-instance-1'), {'extra-data': 'extra-data'})
+            self.assertEqual(asset_instance['data'], {'extra-data': 'extra-data'})
+
+    def test_get_sequence_from_scene(self):
+        with requests_mock.mock() as mock:
+            mock.get(
+                gazu.client.get_full_url("data/sequences/%s" % fakeid('sequence-1')),
+                text=json.dumps(
+                    {"name": "sequence-1", "project_id": "project-1", 'id': fakeid('sequence-1')}
+                ),
+            )
+            scene = {"id": fakeid('scene-1'), "parent_id": fakeid('sequence-1')}
+            sequence = gazu.scene.get_sequence_from_scene(scene)
+            self.assertEqual(sequence["name"], "sequence-1")
