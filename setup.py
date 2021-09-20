fa0855f9ef409f85f60cd475ad16f644b512218d
Antoine Boellinger <aboellinger@hotmail.com>
2021-09-20 12:24:24 +0200

    Fix requirements after socketio client refactoring.
    Refactored to get a single source of truth on the requirements.
    Had to fix issues related to the import of gazu in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
+import ast
+import re
+import os
+
 from setuptools import setup
 
-from gazu.__version__ import __version__
+
+# We hack our way into getting the version from gazu without imports :
+# running gazu/__init__.py would cause errors due to the import of dependencies yet to be installed
+versionpath = os.path.join(os.path.dirname(__file__), "gazu/__version__.py")
+with open(versionpath, "r") as file:
+    __version__ = ast.literal_eval(re.search('__version__ *= *(\S+)', file.read()).group(1))
 
 setup()
