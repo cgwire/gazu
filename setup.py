39153a430e08bc3afe46c747773beb4295707b41
Evan Blaudy <evan@cg-wire.com>
2021-09-20 22:39:52 +0200

    [qa] drop support for python3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from distutils.util import convert_path
 # Get version without sourcing gazu module
 # (to avoid importing dependencies yet to be installed)
 main_ns = {}
-with open(convert_path('gazu/__version__.py')) as ver_file:
+with open(convert_path("gazu/__version__.py")) as ver_file:
     exec(ver_file.read(), main_ns)
 
-setup(version=main_ns['__version__'])
+setup(
+    version=main_ns["__version__"],
+    python_requires=">2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+)
