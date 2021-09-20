e006631728ca5238970e5243739c96377b87f8d1
Antoine Boellinger <aboellinger@hotmail.com>
2021-09-20 13:56:18 +0200

    Fix setuptools install on python 2

--- setup.py
+++ setup.py
@@ -12,4 +12,4 @@ versionpath = os.path.join(os.path.dirname(__file__), "gazu/__version__.py")
 with open(versionpath, "r") as file:
     __version__ = ast.literal_eval(re.search('__version__ *= *(\S+)', file.read()).group(1))
 
-setup()
+setup(version=__version__)
