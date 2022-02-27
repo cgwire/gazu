40050866af2d04aee3f820c2e30a49c25cf08ac2
Frank Rousseau <frank.rousseau@free.fr>
2022-02-27 23:36:21 +0100

    Configure specifications for DCCUtils

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('/home/frank/Projets/cgwire/gazu'))
+sys.path.insert(0, os.path.abspath('/home/frank/Projets/cgwire/dccutils'))
 
 
 # -- Project information -----------------------------------------------------
