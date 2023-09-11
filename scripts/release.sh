c683b16e4ad95483768bf09077c064f5decfc437
Evan Blaudy <evan@cg-wire.com>
2023-09-12 01:25:33 +0200

    [release] change release scripts to work with github actions

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,7 @@
-release_number=0.8.27
-twine --help || exit 1
-echo "__version__ = \"$release_number\"" > gazu/__version__.py
-git add gazu/__version__.py
-git commit gazu/__version__.py -m $release_number
 git pull --rebase origin master
+last_release_number=$(python -c "from gazu import __version__; print(__version__)")
+release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+echo "__version__ = \"$release_number\"" > gazu/__version__.py
+git commit gazu/__version__.py -m $release_number
 git tag v$release_number
 git push origin master --tag
-python setup.py bdist_wheel --universal
-twine upload dist/gazu-$release_number-py2.py3-none-any.whl
