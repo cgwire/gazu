2fd16aacbc23cf21ec539554a46f08ee2c57dc5c
Frank Rousseau <frank.rousseau@free.fr>
2023-09-14 15:49:33 +0200

    [persons] Improve get_by_full_name function
    Allow to use first and last name

--- gazu/person.py
+++ gazu/person.py
@@ -122,7 +122,12 @@ def get_person_by_email(email, client=default):
 
 
 @cache
-def get_person_by_full_name(full_name, client=default):
+def get_person_by_full_name(
+    full_name,
+    first_name=None,
+    last_name=None,
+    client=default
+):
     """
     Args:
         full_name (str): User's full name
@@ -130,19 +135,26 @@ def get_person_by_full_name(full_name, client=default):
     Returns:
         dict: Person corresponding to given name.
     """
-    if " " in full_name:
-        first_name, last_name = full_name.lower().split(" ")
+    if first_name is not None and last_name is not None:
+        return raw.fetch_first(
+            "persons",
+            {"first_name": first_name, "last_name": last_name},
+            client=client,
+        )
     else:
-        first_name, last_name = full_name.lower().strip(), ""
-    for person in all_persons():
-        is_right_first_name = (
-            first_name == person["first_name"].lower().strip()
-        )
-        is_right_last_name = (
-            len(last_name) == 0 or last_name == person["last_name"].lower()
-        )
-        if is_right_first_name and is_right_last_name:
-            return person
+        if " " in full_name:
+            first_name, last_name = full_name.lower().split(" ")
+        else:
+            first_name, last_name = full_name.lower().strip(), ""
+        for person in all_persons():
+            is_right_first_name = (
+                first_name == person["first_name"].lower().strip()
+            )
+            is_right_last_name = (
+                len(last_name) == 0 or last_name == person["last_name"].lower()
+            )
+            if is_right_first_name and is_right_last_name:
+                return person
     return None
 
 
