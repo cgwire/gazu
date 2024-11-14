8e811fa068df94850faed971ea50d228daf3274c
Evan Blaudy <evan@cg-wire.com>
2024-11-14 10:53:35 +0100

    [release] change master to main branch

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 set -e
-git pull --rebase origin master
+git pull --rebase origin main
 last_release_number=$(python -c "from gazu import __version__; print(__version__)")
 release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 echo "__version__ = \"$release_number\"" > gazu/__version__.py
 git commit gazu/__version__.py -m $release_number
 git tag v$release_number
-git push origin master --tag
+git push origin main --tag
