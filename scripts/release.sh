b1caf403a2655265144dc293c0ae679a345e300b
Evan Blaudy <evan@cg-wire.com>
2025-06-17 11:44:53 +0200

    [release] fix tags not pushed

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,5 @@ last_release_number=$(python -c "from gazu import __version__; print(__version__
 release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 echo "__version__ = \"$release_number\"" > gazu/__version__.py
 git commit gazu/__version__.py -m $release_number
-git tag v$release_number
+git tag -a -m "Release v$release_number" v$release_number
 git push origin main --follow-tags
