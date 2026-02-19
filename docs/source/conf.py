e7cf9a0ed84f04d512e289655a064232855dc62e
Frank Rousseau <frank@cg-wire.com>
2026-02-19 10:57:29 +0100

    [qa] Up Gazu version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = "2023, CGWire"
 author = "CGWire"
 
 # The short X.Y version
-version = "0.9.17"
+version = "1.1.4"
 # The full version, including alpha/beta/rc tags
-release = "0.9.17"
+release = "1.1.4"
 
 
 # -- General configuration ---------------------------------------------------
